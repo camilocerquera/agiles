424b05cd40dfd211d55e87d95170de4aab826aa0
camilocerquera <jc.cerquera10@uniandes.edu.co>
2015-08-07 23:55:44 -0500

    Configuracion DB postgress heroku

--- proyectoOvas/proyectoOvas/settings.py
+++ proyectoOvas/proyectoOvas/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,22 +77,23 @@ WSGI_APPLICATION = 'proyectoOvas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'OVAS_DB_2',
-        'USER': 'postgres',
-        'PASSWORD': '********',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'OVAS_DB_2',
+#        'USER': 'postgres',
+#        'PASSWORD': '********',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-co'
+LANGUAGE_CODE = 'es-co'
 
 TIME_ZONE = 'America/Bogota'
 
